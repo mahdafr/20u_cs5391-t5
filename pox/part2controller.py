a7b71749ab7101c83684c4073f07a6c3952b9fa9
Mahdokht Af <mmafravi@gmail.com>
2020-07-21 14:49:11 -0600

    added GoogleDrive files

---
+++ pox/part2controller.py
@@ -0,0 +1,47 @@
+# Part 2 of UWCSE's Project 3
+#
+# based on Lab 4 from UCSC's Networking Class
+# which is based on of_tutorial by James McCauley
+
+from pox.core import core
+import pox.openflow.libopenflow_01 as of
+
+log = core.getLogger()
+
+class Firewall (object):
+  """
+  A Firewall object is created for each switch that connects.
+  A Connection object for that switch is passed to the __init__ function.
+  """
+  def __init__ (self, connection):
+    # Keep track of the connection to the switch so that we can
+    # send it messages!
+    self.connection = connection
+
+    # This binds our PacketIn event listener
+    connection.addListeners(self)
+
+    #add switch rules here
+
+  def _handle_PacketIn (self, event):
+    """
+    Packets not handled by the router rules will be
+    forwarded to this method to be handled by the controller
+    """
+
+    packet = event.parsed # This is the parsed packet data.
+    if not packet.parsed:
+      log.warning("Ignoring incomplete packet")
+      return
+
+    packet_in = event.ofp # The actual ofp_packet_in message.
+    print ("Unhandled packet :" + str(packet.dump()))
+
+def launch ():
+  """
+  Starts the component
+  """
+  def start_switch (event):
+    log.debug("Controlling %s" % (event.connection,))
+    Firewall(event.connection)
+  core.openflow.addListenerByName("ConnectionUp", start_switch)
---
+++ topos/part1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.cli import CLI
+
+class part1_topo(Topo):
+    
+    def build(self):
+        pass
+        #switch1 = self.addSwitch('switchname')
+        #host1 = self.addHost('hostname')
+        #self.addLink(hostname,switchname)
+
+topos = {'part1' : part1_topo}
+
+if __name__ == '__main__':
+    t = part1_topo()
+    net = Mininet (topo=t)
+    net.start()
+    CLI(net)
+    net.stop()
